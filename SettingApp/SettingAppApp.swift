479af9de1af9481bb1f511a79d7f1a75edcbaf01
Jia <71865277+jangjia01234@users.noreply.github.com>
2024-03-09 10:28:54 +0900

    Initial Commit

---
+++ SettingApp/SettingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SettingAppApp.swift
+//  SettingApp
+//
+//  Created by Jia Jang on 3/9/24.
+//
+
+import SwiftUI
+
+@main
+struct SettingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
