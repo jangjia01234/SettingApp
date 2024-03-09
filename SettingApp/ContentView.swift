94dbf508262cca774132a21aba3b2b70aa8622cc
Jia <71865277+jangjia01234@users.noreply.github.com>
2024-03-09 16:41:17 +0900

    :art: 설정앱 UI 구현

--- SettingApp/ContentView.swift
+++ SettingApp/ContentView.swift
@@ -3,19 +3,20 @@ import SwiftUI
 struct ContentView: View {
     
     @State private var searchText: String = ""
+    @State private var isToggleOn: Bool = false
     
     var body: some View {
         NavigationView {
             List {
-                ZStack(alignment: .leading) {
-                    Image(systemName: "magnifyingglass")
-                        .foregroundColor(.gray)
-                    TextField("검색", text: $searchText)
-                        .searchable(text: $searchText, prompt: "검색")
-                        .padding(.leading, 30)
-                        
-                }
-                .onTapGesture { hideKeyboard() }
+//                ZStack(alignment: .leading) {
+//                    Image(systemName: "magnifyingglass")
+//                        .foregroundColor(.gray)
+//                    TextField("검색", text: $searchText)
+//                        .searchable(text: $searchText, prompt: "검색")
+//                        .padding(.leading, 30)
+//                        
+//                }
+//                .onTapGesture { hideKeyboard() }
                    
                 Section {
                     HStack {
@@ -29,44 +30,250 @@ struct ContentView: View {
                                 .foregroundColor(.white)
                         }
                         
-                        VStack(alignment: .leading) {
-                            Section {
-                                Text("Jia")
-                                    .font(.title2)
-                                    .fontWeight(.semibold)
-                                Text("Apple ID, iCloud+, 미디어 및 구입 항목")
-                                    .font(.caption)
+                        HStack {
+                            VStack(alignment: .leading) {
+                                Section {
+                                    Text("Jia")
+                                        .font(.title2)
+                                        .fontWeight(.semibold)
+                                    Text("Apple ID, iCloud+, 미디어 및 구입 항목")
+                                        .font(.caption)
+                                }
                             }
+                            Spacer()
+                            Image(systemName: "chevron.right")
+                                .foregroundColor(.gray)
                         }
                         .padding(.leading, 10)
                     }
                     
-                    Section {
-                        Text("Apple Arcade 3개월 무료")
-                            .font(.subheadline)
+                    HStack {
+                        Section {
+                            Text("Apple Arcade 3개월 무료")
+                                .font(.subheadline)
+                        }
+                        Spacer()
+                        Image(systemName: "chevron.right")
+                            .foregroundColor(.gray)
                     }
                 }
                 
                 Section {
-                    Text("에어플레인 모드")
-                    Text("Wi-Fi")
-                    Text("Bluetooth")
-                    Text("셀룰러")
-                    Text("개인용 핫스팟")
+                    
+                    HStack {
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 5)
+                                .fill(Color.orange)
+                                .frame(width: 30, height: 30)
+                            Image(systemName: "airplane")
+                                .foregroundColor(.white)
+                        }
+                        Text("에어플레인 모드")
+                            .padding(.leading, 5)
+                        Spacer()
+                        Toggle(isOn: $isToggleOn) {
+                            // 토글 코드
+                        }
+                    }
+                    
+                    HStack {
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 5)
+                                .fill(Color.blue)
+                                .frame(width: 30, height: 30)
+                            Image(systemName: "wifi")
+                                .foregroundColor(.white)
+                        }
+                        Text("Wi-Fi")
+                            .padding(.leading, 5)
+                        Spacer()
+                        Text("barbet_2F")
+                            .foregroundColor(.gray)
+                        Image(systemName: "chevron.right")
+                            .foregroundColor(.gray)
+                    }
+                    
+                    HStack {
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 5)
+                                .fill(Color.blue)
+                                .frame(width: 30, height: 30)
+                            
+                            // bluetooth 로고로 변경 필요
+                            Image(systemName: "bolt.fill")
+                                .foregroundColor(.white)
+                        }
+                        Text("Bluetooth")
+                            .padding(.leading, 5)
+                        Spacer()
+                        Text("연결 안 됨")
+                            .foregroundColor(.gray)
+                        Image(systemName: "chevron.right")
+                            .foregroundColor(.gray)
+                    }
+                    
+                    HStack {
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 5)
+                                .fill(Color.green)
+                                .frame(width: 30, height: 30)
+                            // celular 로고로 변경 필요
+                            Image(systemName: "antenna.radiowaves.left.and.right")
+                                .foregroundColor(.white)
+                        }
+                        Text("셀룰러")
+                            .padding(.leading, 5)
+                        Spacer()
+                        Image(systemName: "chevron.right")
+                            .foregroundColor(.gray)
+                    }
+                    
+                    HStack {
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 5)
+                                .fill(Color.green)
+                                .frame(width: 30, height: 30)
+                            Image(systemName: "personalhotspot")
+                                .foregroundColor(.white)
+                        }
+                        Text("개인용 핫스팟")
+                            .padding(.leading, 5)
+                        Spacer()
+                        Text("끔")
+                            .foregroundColor(.gray)
+                        Image(systemName: "chevron.right")
+                            .foregroundColor(.gray)
+                    }
+                    
                 }
                 
                 Section {
-                    Text("알림")
-                    Text("사운드 및 햅틱")
-                    Text("집중 모드")
-                    Text("스크린 타임")
+                    HStack {
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 5)
+                                .fill(Color.red)
+                                .frame(width: 30, height: 30)
+                            // bell + dot 로고로 변경 필요
+                            Image(systemName: "bell")
+                                .foregroundColor(.white)
+                        }
+
+                        Text("알림")
+                            .padding(.leading, 5)
+                        Spacer()
+                        Image(systemName: "chevron.right")
+                            .foregroundColor(.gray)
+                    }
+                    
+                    HStack {
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 5)
+                                .fill(Color.red)
+                                .frame(width: 30, height: 30)
+                            Image(systemName: "speaker.wave.3.fill")
+                                .foregroundColor(.white)
+                        }
+                        Text("사운드 및 햅틱")
+                            .padding(.leading, 5)
+                        Spacer()
+                        Image(systemName: "chevron.right")
+                            .foregroundColor(.gray)
+                    }
+                    
+                    HStack {
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 5)
+                                .fill(Color.indigo)
+                                .frame(width: 30, height: 30)
+                            Image(systemName: "moon.fill")
+                                .foregroundColor(.white)
+                        }
+                        Text("집중 모드")
+                            .padding(.leading, 5)
+                        Spacer()
+                        Image(systemName: "chevron.right")
+                            .foregroundColor(.gray)
+                    }
+                    
+                    HStack {
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 5)
+                                .fill(Color.indigo)
+                                .frame(width: 30, height: 30)
+                            Image(systemName: "hourglass")
+                                .foregroundColor(.white)
+                        }
+                        Text("스크린 타임")
+                            .padding(.leading, 5)
+                        Spacer()
+                        Image(systemName: "chevron.right")
+                            .foregroundColor(.gray)
+                    }
+                    
                 }
                 
                 Section {
-                    Text("일반")
-                    Text("제어 센터")
-                    Text("동작 버튼")
-                    Text("디스플레이 및 밝기")
+                    HStack {
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 5)
+                                .fill(Color.gray)
+                                .frame(width: 30, height: 30)
+                            Image(systemName: "gear")
+                                .foregroundColor(.white)
+                        }
+                        Text("일반")
+                            .padding(.leading, 5)
+                        Spacer()
+                        Image(systemName: "chevron.right")
+                            .foregroundColor(.gray)
+                    }
+                    
+                    HStack {
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 5)
+                                .fill(Color.gray)
+                                .frame(width: 30, height: 30)
+                            // 제어 센터 심볼로 변경 필요
+                            Image(systemName: "distribute.vertical.center")
+                                .foregroundColor(.white)
+                        }
+                        Text("제어 센터")
+                            .padding(.leading, 5)
+                        Spacer()
+                        Image(systemName: "chevron.right")
+                            .foregroundColor(.gray)
+                    }
+                    
+                    HStack {
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 5)
+                                .fill(Color.blue)
+                                .frame(width: 30, height: 30)
+                            Image(systemName: "button.vertical.left.press")
+                                .foregroundColor(.white)
+                        }
+                        // 동작 버튼 심볼로 변경 필요
+                        Text("동작 버튼")
+                            .padding(.leading, 5)
+                        Spacer()
+                        Image(systemName: "chevron.right")
+                            .foregroundColor(.gray)
+                    }
+                    
+                    HStack {
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 5)
+                                .fill(Color.blue)
+                                .frame(width: 30, height: 30)
+                            Image(systemName: "sun.max.fill")
+                                .foregroundColor(.white)
+                        }
+                        Text("디스플레이 및 밝기")
+                            .padding(.leading, 5)
+                        Spacer()
+                        Image(systemName: "chevron.right")
+                            .foregroundColor(.gray)
+                    }
                 }
             }
             .navigationTitle(Text("설정"))
