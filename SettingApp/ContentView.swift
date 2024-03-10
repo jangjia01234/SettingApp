7378ddb0233bc87ee45ae1d1f9bb7a41f2b62a1f
Jia <71865277+jangjia01234@users.noreply.github.com>
2024-03-11 02:10:39 +0900

    :recycle: 코드 리팩토링

--- SettingApp/ContentView.swift
+++ SettingApp/ContentView.swift
@@ -1,7 +1,6 @@
 import SwiftUI
 
 struct ContentView: View {
-    
     @State private var searchText: String = ""
     @State private var isToggleOn: Bool = false
     
@@ -9,253 +8,209 @@ struct ContentView: View {
         NavigationView {
             List {
                 Section {
-                    HStack {
-                        ZStack {
-                            Circle()
-                                .frame(width: 60)
-                                .foregroundColor(.gray)
-                            Text("J")
-                                .font(.title)
-                                .fontWeight(.semibold)
-                                .foregroundColor(.white)
+                    profileCell()
+                }
+                
+                Section {
+                    basicCell(
+                        colorName: .orange,
+                        iconName: "airplane",
+                        cellName: "에어플레인 모드",
+                        subName: "toggle") {
+                            Text("nothing")
                         }
-                        
-                        HStack {
-                            VStack(alignment: .leading) {
-                                Section {
-                                    Text("Jia")
-                                        .font(.title2)
-                                        .fontWeight(.semibold)
-                                    Text("Apple ID, iCloud+, 미디어 및 구입 항목")
-                                        .font(.caption)
-                                }
-                            }
-                            Spacer()
-                            Image(systemName: "chevron.right")
-                                .foregroundColor(.gray)
-                        }
-                        .padding(.leading, 10)
+                    
+                    basicCell(
+                        colorName: .blue,
+                        iconName: "wifi",
+                        cellName: "Wi-Fi",
+                        subName: "public_2F") {
+                            Text("Detailed page")
                     }
                     
-                    HStack {
-                        Section {
-                            Text("Apple Arcade 3개월 무료")
-                                .font(.subheadline)
-                        }
-                        Spacer()
-                        Image(systemName: "chevron.right")
-                            .foregroundColor(.gray)
+                    basicCell(
+                        colorName: .green,
+                        iconName: "antenna.radiowaves.left.and.right",
+                        cellName: "셀룰러",
+                        subName: "") {
+                            Text("Detailed page")
+                    }
+                    
+                    basicCell(
+                        colorName: .green,
+                        iconName: "personalhotspot",
+                        cellName: "개인용 핫스팟",
+                        subName: "끔") {
+                            Text("Detailed page")
                     }
                 }
                 
                 Section {
-                    
-                    HStack {
-                        ZStack {
-                            RoundedRectangle(cornerRadius: 5)
-                                .fill(Color.orange)
-                                .frame(width: 30, height: 30)
-                            Image(systemName: "airplane")
-                                .foregroundColor(.white)
-                        }
-                        Text("에어플레인 모드")
-                            .padding(.leading, 5)
-                        Spacer()
-                        Toggle(isOn: $isToggleOn) {
-                            // 토글 코드
-                        }
+                    basicCell(
+                        colorName: .red,
+                        iconName: "bell.badge.fill",
+                        cellName: "알림",
+                        subName: "") {
+                            Text("Detailed page")
                     }
                     
-                    HStack {
-                        ZStack {
-                            RoundedRectangle(cornerRadius: 5)
-                                .fill(Color.blue)
-                                .frame(width: 30, height: 30)
-                            Image(systemName: "wifi")
-                                .foregroundColor(.white)
-                        }
-                        Text("Wi-Fi")
-                            .padding(.leading, 5)
-                        Spacer()
-                        Text("public_2F")
-                            .foregroundColor(.gray)
-                        Image(systemName: "chevron.right")
-                            .foregroundColor(.gray)
+                    basicCell(
+                        colorName: .red,
+                        iconName: "speaker.wave.3.fill",
+                        cellName: "사운드 및 햅틱",
+                        subName: "") {
+                            Text("Detailed page")
                     }
                     
-                    HStack {
-                        ZStack {
-                            RoundedRectangle(cornerRadius: 5)
-                                .fill(Color.green)
-                                .frame(width: 30, height: 30)
-                            Image(systemName: "antenna.radiowaves.left.and.right")
-                                .foregroundColor(.white)
-                        }
-                        Text("셀룰러")
-                            .padding(.leading, 5)
-                        Spacer()
-                        Image(systemName: "chevron.right")
-                            .foregroundColor(.gray)
+                    basicCell(
+                        colorName: .indigo,
+                        iconName: "moon.fill",
+                        cellName: "집중 모드",
+                        subName: "") {
+                            Text("Detailed page")
                     }
                     
-                    HStack {
-                        ZStack {
-                            RoundedRectangle(cornerRadius: 5)
-                                .fill(Color.green)
-                                .frame(width: 30, height: 30)
-                            Image(systemName: "personalhotspot")
-                                .foregroundColor(.white)
-                        }
-                        Text("개인용 핫스팟")
-                            .padding(.leading, 5)
-                        Spacer()
-                        Text("끔")
-                            .foregroundColor(.gray)
-                        Image(systemName: "chevron.right")
-                            .foregroundColor(.gray)
+                    basicCell(
+                        colorName: .indigo,
+                        iconName: "hourglass",
+                        cellName: "스크린 타임",
+                        subName: "") {
+                            Text("Detailed page")
                     }
-                    
                 }
                 
                 Section {
-                    HStack {
-                        ZStack {
-                            RoundedRectangle(cornerRadius: 5)
-                                .fill(Color.red)
-                                .frame(width: 30, height: 30)
-                            Image(systemName: "bell.badge.fill")
-                                .foregroundColor(.white)
-                        }
-
-                        Text("알림")
-                            .padding(.leading, 5)
-                        Spacer()
-                        Image(systemName: "chevron.right")
-                            .foregroundColor(.gray)
+                    basicCell(
+                        colorName: .gray,
+                        iconName: "gear",
+                        cellName: "일반",
+                        subName: "") {
+                            Text("Detailed page")
                     }
                     
-                    HStack {
-                        ZStack {
-                            RoundedRectangle(cornerRadius: 5)
-                                .fill(Color.red)
-                                .frame(width: 30, height: 30)
-                            Image(systemName: "speaker.wave.3.fill")
-                                .foregroundColor(.white)
-                        }
-                        Text("사운드 및 햅틱")
-                            .padding(.leading, 5)
-                        Spacer()
-                        Image(systemName: "chevron.right")
-                            .foregroundColor(.gray)
+                    basicCell(
+                        colorName: .blue,
+                        iconName: "sun.max.fill",
+                        cellName: "디스플레이 및 밝기",
+                        subName: "") {
+                            Text("Detailed page")
                     }
                     
-                    HStack {
-                        ZStack {
-                            RoundedRectangle(cornerRadius: 5)
-                                .fill(Color.indigo)
-                                .frame(width: 30, height: 30)
-                            Image(systemName: "moon.fill")
-                                .foregroundColor(.white)
-                        }
-                        Text("집중 모드")
-                            .padding(.leading, 5)
-                        Spacer()
-                        Image(systemName: "chevron.right")
-                            .foregroundColor(.gray)
+                    basicCell(
+                        colorName: .blue,
+                        iconName: "sun.max.fill",
+                        cellName: "디스플레이 및 밝기",
+                        subName: "") {
+                            Text("Detailed page")
                     }
                     
-                    HStack {
-                        ZStack {
-                            RoundedRectangle(cornerRadius: 5)
-                                .fill(Color.indigo)
-                                .frame(width: 30, height: 30)
-                            Image(systemName: "hourglass")
-                                .foregroundColor(.white)
-                        }
-                        Text("스크린 타임")
-                            .padding(.leading, 5)
-                        Spacer()
-                        Image(systemName: "chevron.right")
-                            .foregroundColor(.gray)
+                    basicCell(
+                        colorName: .blue,
+                        iconName: "accessibility",
+                        cellName: "손쉬운 사용",
+                        subName: "") {
+                            Text("Detailed page")
                     }
                     
-                }
-                
-                Section {
-                    HStack {
-                        ZStack {
-                            RoundedRectangle(cornerRadius: 5)
-                                .fill(Color.gray)
-                                .frame(width: 30, height: 30)
-                            Image(systemName: "gear")
-                                .foregroundColor(.white)
-                        }
-                        Text("일반")
-                            .padding(.leading, 5)
-                        Spacer()
-                        Image(systemName: "chevron.right")
-                            .foregroundColor(.gray)
-                    }
-                    
-                    HStack {
-                        ZStack {
-                            RoundedRectangle(cornerRadius: 5)
-                                .fill(Color.blue)
-                                .frame(width: 30, height: 30)
-                            Image(systemName: "sun.max.fill")
-                                .foregroundColor(.white)
-                        }
-                        Text("디스플레이 및 밝기")
-                            .padding(.leading, 5)
-                        Spacer()
-                        Image(systemName: "chevron.right")
-                            .foregroundColor(.gray)
-                    }
-                    
-                    HStack {
-                        ZStack {
-                            RoundedRectangle(cornerRadius: 5)
-                                .fill(Color.blue)
-                                .frame(width: 30, height: 30)
-                            
-                            Image(systemName: "accessibility")
-                                .foregroundColor(.white)
-                        }
-                        Text("손쉬운 사용")
-                            .padding(.leading, 5)
-                        Spacer()
-                        Image(systemName: "chevron.right")
-                            .foregroundColor(.gray)
-                    }
-                    
-                    HStack {
-                        ZStack {
-                            RoundedRectangle(cornerRadius: 5)
-                                .fill(Color.green)
-                                .frame(width: 30, height: 30)
-                            Image(systemName: "battery.100percent")
-                                .foregroundColor(.white)
-                        }
-                        Text("배터리")
-                            .padding(.leading, 5)
-                        Spacer()
-                        Image(systemName: "chevron.right")
-                            .foregroundColor(.gray)
+                    basicCell(
+                        colorName: .green,
+                        iconName: "battery.100percent",
+                        cellName: "배터리",
+                        subName: "") {
+                            Text("Detailed page")
                     }
                 }
             }
             .navigationTitle(Text("설정"))
         }
     }
-}
-
-#if canImport(UIKit)
-extension View {
-    func hideKeyboard() {
-        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    
+    @ViewBuilder
+    private func profileCell() -> some View {
+        HStack {
+            ZStack {
+                Circle()
+                    .frame(width: 60)
+                    .foregroundColor(.gray)
+                Text("J")
+                    .font(.title)
+                    .fontWeight(.semibold)
+                    .foregroundColor(.white)
+            }
+            
+            NavigationLink(destination: Text("Detailed page")) {
+                HStack {
+                    VStack(alignment: .leading) {
+                        Section {
+                            Text("Jia")
+                                .font(.title2)
+                                .fontWeight(.semibold)
+                            Text("Apple ID, iCloud+, 미디어 및 구입 항목")
+                                .font(.caption)
+                        }
+                    }
+                }
+                .padding(.leading, 10)
+            }
+        }
+        
+        HStack {
+            Section {
+                NavigationLink(destination: Text("Detailed page")) {
+                    Text("Apple Arcade 3개월 무료")
+                        .font(.subheadline)
+                }
+            }
+        }
+    }
+    
+    @ViewBuilder
+    private func basicCell<V: View>(
+        colorName: Color,
+        iconName: String,
+        cellName: String,
+        subName: String?,
+        destination: @escaping () -> V
+    ) -> some View {
+        HStack {
+            Image(systemName: iconName)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 20, height: 20)
+                .padding(6)
+                .background(colorName)
+                .foregroundColor(.white)
+                .cornerRadius(6)
+            
+            if let subName = subName {
+                if subName == "toggle" {
+                    Text(cellName)
+                        .padding(.leading, 5)
+                    Spacer()
+                    Toggle(isOn: $isToggleOn) {
+                        // 토글 관련 코드
+                    }
+                } else {
+                    NavigationLink() {
+                        destination()
+                    } label: {
+                        HStack {
+                            Text(cellName)
+                                .padding(.leading, 5)
+                            Spacer()
+                            Text(subName)
+                                .foregroundColor(.gray)
+                        }
+                    }
+                }
+            } else {
+                NavigationLink(cellName) {
+                    destination()
+                }
+            }
+        }
     }
 }
-#endif
 
 #Preview {
     ContentView()
